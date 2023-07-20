bed0cf6366bc1236b690bb2bb9f8dc5c42234e2d
josafat <josa221001@gmail.com>
2023-07-19 23:33:42 -0600

    add: menu en dashboard

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,28 +1,51 @@
-import { useContext } from 'react'
-import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
+import { useState } from "react";
+import { Box, Button, Flex, Grid, Heading, Text } from "@chakra-ui/react";
 
-import { AuthContext } from '../context/AuthContext'
-import useProfile from '../hooks/useProfile'
-import { logout } from '../services/auth'
+import { AuthContext } from "../context/AuthContext";
+import useProfile from "../hooks/useProfile";
+import { logout } from "../services/auth";
 
 const Welcome = () => {
-  const userProfile = useProfile()
+  const userProfile = useProfile();
+  const [isAsideVisible, setAsideVisible] = useState(false);
 
-  const handleLogout = async () => await logout()
+  const handleLogout = async () => await logout();
+
+  const toggleAside = () => {
+    setAsideVisible(!isAsideVisible);
+  };
 
   return (
-    <Flex mt={4} pr={8} pl={8} flexDirection={'row'} justifyContent={'space-between'}>
-      <Box>
-        <Heading as='h3' size='md' mb={2}>
-          Welcome
-        </Heading>
-        <Text fontSize='sm' color='#718096' fontWeight={'600'}>
-          { userProfile ? userProfile.username : 'Sin Datos' }
-        </Text>
+    <Grid
+      templateColumns={{ base: "1fr", md: "1fr 2fr" }}
+      templateAreas={{ base: '"main" "navbar"', md: '"navbar main"' }}
+      mt={4}
+      pr={{ base: 0, md: 8 }}
+      pl={{ base: 0, md: 8 }}
+      gap={4}
+    >
+      {/* Main content (2/3) */}
+      <Box gridArea="main">
+        {/* Your main content goes here */}
+        <Button onClick={toggleAside}>Toggle Aside</Button>
       </Box>
-      <Button onClick={handleLogout}>Logout</Button>
-    </Flex>
-  )
-}
 
-export default Welcome
+      {/* Aside (1/3) */}
+      {isAsideVisible && (
+        <Box gridArea="navbar">
+          <Heading as="h3" size="md" mb={2}>
+            Welcome
+          </Heading>
+          <Text fontSize="sm" color="#718096" fontWeight="600">
+            {userProfile ? userProfile.username : "Sin Datos"}
+          </Text>
+          <Button mt={4} onClick={handleLogout}>
+            Logout
+          </Button>
+        </Box>
+      )}
+    </Grid>
+  );
+};
+
+export default Welcome;
