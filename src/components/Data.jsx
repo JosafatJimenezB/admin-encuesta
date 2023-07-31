5177e4617fbe3824b07497ac94887aa838937a72
josafat <josa221001@gmail.com>
2023-07-30 19:56:05 -0600

    add: agregando boton de descarga de datos

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,11 +1,13 @@
-import { Flex, Text, Container, Spacer, Box } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Flex, Text, Container, Button, Box, Spacer } from "@chakra-ui/react";
 import questions from "./questions";
 import QuestionResponse from "./QuestionResponse";
 import QuestionChart from "./QuestionChart";
+import Papa from "papaparse";
 
 const Data = () => {
   const [data, setData] = useState([]);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -15,6 +17,7 @@ const Data = () => {
         );
         const data = await response.json();
         setData(data);
+        setIsDataLoaded(true); // Marcar que los datos se han cargado
       } catch (error) {
         console.log("Error al obtener los datos:", error);
       }
@@ -22,11 +25,77 @@ const Data = () => {
     fetchData();
   }, []);
 
+  const handleDownloadCSV = () => {
+    const csvData = prepareCSVData(data);
+    const csv = Papa.unparse(csvData);
+
+    // Crear y descargar el archivo CSV
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("download", "data.csv");
+    link.style.visibility = "hidden";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const prepareCSVData = (data) => {
+    const csvData = data.map((item) => {
+      return [
+        item.createdAt,
+        item.ubication.lat,
+        item.ubication.long,
+        item.responses[0].answer1,
+        item.responses[0].answer2,
+        item.responses[0].answer3,
+        item.responses[0].answer4,
+        item.responses[0].answer5,
+        item.responses[0].answer6,
+        item.responses[0].answer7,
+      ];
+    });
+
+    // Agregar una fila para los encabezados del CSV (opcional)
+    csvData.unshift([
+      "Fecha de creación",
+      "Latitud",
+      "Longitud",
+      "Respuesta 1",
+      "Respuesta 2",
+      "Respuesta 3",
+      "Respuesta 4",
+      "Respuesta 5",
+      "Respuesta 6",
+      "Respuesta 7",
+    ]);
+
+    return csvData;
+  };
+
   return (
     <Flex w={"full"} p={0} m={0} flexDir={"column"}>
       <Text fontSize={{ base: "24px", md: "40px", lg: "56px" }}>
         Estadísticas de los datos
       </Text>
+      <Container
+        maxW={"container.xl"}
+        w={"full"}
+        p={0}
+        m={0}
+        flexDir={"column"}
+        align={"center"}
+      >
+        <Button
+          colorScheme="blue"
+          onClick={handleDownloadCSV}
+          disabled={!isDataLoaded}
+        >
+          Descargar datos en CSV
+        </Button>
+      </Container>
+
       <Flex
         w={"full"}
         border={"4px"}
