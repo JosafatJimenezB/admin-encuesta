64775431a2044562d73ec573a0c1223a2aecd15d
josafat <josa221001@gmail.com>
2023-08-01 15:49:08 -0600

    add: login dinamico con el signup

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Flex,
   Heading,
@@ -6,19 +7,31 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-import MagicLinkForm from "./MagicLinkForm";
+import SignUpForm from "../components/SignUpForm";
 import LoginForm from "./LoginForm";
 
 const LoginPage = () => {
+  const [currentSection, setCurrentSection] = useState("login");
+
+  const handleMenuClick = (section) => {
+    setCurrentSection(section);
+  };
+
   return (
     <Flex
+      w={"full"}
       minH={"100vh"}
       align={"center"}
       justify={"center"}
       bg={useColorModeValue("gray.50", "blue.800")}
     >
       <Stack whiteSpace={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <LoginForm />
+        {currentSection === "login" && (
+          <LoginForm onMenuClick={handleMenuClick} />
+        )}
+        {currentSection === "signup" && (
+          <SignUpForm onMenuClick={handleMenuClick} />
+        )}
       </Stack>
     </Flex>
   );
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,12 +1,16 @@
 import {
+  Box,
   Button,
   FormControl,
   FormLabel,
   Heading,
   Input,
+  Link,
   Stack,
   InputGroup,
+  Text,
   InputRightElement,
+  useColorModeValue,
 } from "@chakra-ui/react";
 
 import useForm from "../hooks/useForm";
@@ -18,7 +22,7 @@ const initialState = {
   passwordr: "",
 };
 
-const SingUpForm = () => {
+const SingUpForm = ({ onMenuClick }) => {
   const { formValues, handleInputChange, reset } = useForm(initialState);
 
   const handleSubmit = async (e) => {
@@ -42,50 +46,78 @@ const SingUpForm = () => {
 
   return (
     <>
-      <Heading fontSize="2xl" mb="15px">
-        Registro
-      </Heading>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          <FormControl id="fullName">
-            <FormLabel>Nombre</FormLabel>
-            <Input
-              type="text"
-              name="fullName"
-              value={formValues.fullName}
-              onChange={handleInputChange}
-            />
-          </FormControl>
-          <FormControl id="emailr">
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              name="emailr"
-              value={formValues.emailr}
-              onChange={handleInputChange}
-            />
-          </FormControl>
-          <FormControl id="passwordr">
-            <FormLabel>Password</FormLabel>
-            <InputGroup>
+      <Box
+        rounded={"lg"}
+        bg={useColorModeValue("white", "gray.700")}
+        boxShadow={"lg"}
+        p={8}
+      >
+        <Stack align={"center"}>
+          <Heading
+            fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+            color={"blue.700"}
+            mb={6}
+          >
+            Registro
+          </Heading>
+        </Stack>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={4}>
+            <FormControl id="fullName">
+              <FormLabel mb={1} color={"blue.700"} fontWeight={"700"}>
+                Nombre
+              </FormLabel>
               <Input
-                type="password"
-                name="passwordr"
-                value={formValues.passwordr}
+                type="text"
+                name="fullName"
+                value={formValues.fullName}
                 onChange={handleInputChange}
               />
-            </InputGroup>
-          </FormControl>
-          <Button
-            type="submit"
-            loadingText="Submitting"
-            bg={"blue.400"}
-            color={"white"}
-          >
-            Registrarse
-          </Button>
-        </Stack>
-      </form>
+            </FormControl>
+            <FormControl id="emailr">
+              <FormLabel mb={1} color={"blue.700"} fontWeight={"700"}>
+                Email
+              </FormLabel>
+              <Input
+                mb={4}
+                type="email"
+                name="emailr"
+                value={formValues.emailr}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+            <FormControl id="passwordr">
+              <FormLabel mb={1} color={"blue.700"} fontWeight={"700"}>
+                Password
+              </FormLabel>
+              <InputGroup>
+                <Input
+                  mb={4}
+                  type="password"
+                  name="passwordr"
+                  value={formValues.passwordr}
+                  onChange={handleInputChange}
+                />
+              </InputGroup>
+            </FormControl>
+            <Button
+              type="submit"
+              loadingText="Submitting"
+              bg={"blue.600"}
+              color={"white"}
+              _hover={{ bg: "blue.300", color: "white" }}
+            >
+              Registrarse
+            </Button>
+            <Text>
+              Ya tiene una cuenta? {""}
+              <Link color={"blue.400"} onClick={() => onMenuClick("login")}>
+                Iniciar sesión
+              </Link>
+            </Text>
+          </Stack>
+        </form>
+      </Box>
     </>
   );
 };
