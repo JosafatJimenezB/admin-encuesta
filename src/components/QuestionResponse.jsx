eaa4eb4f10a7aacd22841dd50e9b58dd84b9c2eb
Texhnolyze47 <ichigobankai136@gmail.com>
2023-08-03 04:04:48 -0600

    children index

--- src/components/QuestionResponse.jsx
+++ src/components/QuestionResponse.jsx
@@ -1,6 +1,5 @@
 import { Flex, Text, Badge, Box } from '@chakra-ui/react'
 import questions from './questions'
-
 const QuestionResponse = ({ data }) => {
   const getColorForAnswer = (answer) => {
     const colors = {
@@ -39,41 +38,27 @@ const QuestionResponse = ({ data }) => {
     <Flex w={'full'} flexDirection="column" rounded={'lg'} p={4} m={0}>
       {Object.values(answerCounts).map((answers, index) => {
         return (
-          <Box key={index} bg={'blue.100'} p={2} mb={2}>
-            <Flex flexDirection="column">
-              <Text fontSize="xl" fontWeight="bold" mb={2}>
-                {questions[index].question}
-              </Text>
-              {questions[index].responses.map((response, responseIndex) => {
-                const answer = response.answer
-                const text = response.text
-                const count = answers[answer] || 0
-                return (
-                  <Flex
-                    key={responseIndex}
-                    flexDirection="row"
-                    alignItems="center"
+          <Box key={index} bg={'blue.100'} p={8} mt={16} mb={26}>
+            <Text fontSize="xl" fontWeight="bold" mb={2}>
+              {questions[index].question}
+            </Text>
+            {questions[index].responses.map((response, responseIndex) => {
+              const answer = response.answer
+              const text = response.text
+              const count = answers[answer] || 0
+              return (
+                <Text key={responseIndex} fontSize="xl" my={1}>
+                  <Badge
+                    color={'white'}
+                    rounded={'lg'}
+                    bg={`${getColorForAnswer(answer)}.500`}
                   >
-                    <Box mr={2}>
-                      <Badge
-                        color={'white'}
-                        rounded={'md'}
-                        bg={`${getColorForAnswer(answer)}.500`}
-                      >
-                        {answer}
-                      </Badge>
-                    </Box>
-                    <Box>
-                      <Text fontSize="sm" color={'gray.500'}>
-                        {text}
-                      </Text>
-                    </Box>
-
-                    <Box mr={2}>{count}</Box>
-                  </Flex>
-                )
-              })}
-            </Flex>
+                    {answer}
+                  </Badge>{' '}
+                  {text}: {count}
+                </Text>
+              )
+            })}
           </Box>
         )
       })}
