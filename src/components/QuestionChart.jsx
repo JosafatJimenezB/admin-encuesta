26ff3d97f7b3e210efcbbfce20ea3282be00f6d1
Texhnolyze47 <ichigobankai136@gmail.com>
2023-08-04 12:36:22 -0600

    add: colores en las graficas

--- src/components/QuestionChart.jsx
+++ src/components/QuestionChart.jsx
@@ -1,52 +1,52 @@
-import React from "react";
-import { Flex, Text, color } from "@chakra-ui/react";
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar } from "recharts";
-import questions from "./questions";
+import React from 'react'
+import { Flex, Text } from '@chakra-ui/react'
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar,
+  Cell
+} from 'recharts'
+import questions from './questions'
 
 const QuestionChart = ({ data }) => {
+  const colors = ['#00FF00', '#FF0000', '#0000FF']
+
   return (
-    <Flex w={"full"} flexDirection="column" rounded={"lg"} p={4} m={0}>
+    <Flex w={'full'} flexDirection="column" rounded={'lg'} p={4} m={0}>
       {questions.map((question, index) => {
-        const title = question.question;
-        //const colors = ["#FF0000", "#00FF00", "#0000FF"];
+        const title = question.question
         const answerCounts = data.reduce((counts, item) => {
-          const response = item.responses[index];
-          const answerKey = Object.keys(response)[0];
-          const answer = response[answerKey];
-          counts[answer] = (counts[answer] || 0) + 1; // incrementar el conteo de la respuesta
-          return counts;
-        }, {});
+          const response = item.responses[index]
+          const answerKey = Object.keys(response)[0]
+          const answer = response[answerKey]
+          counts[answer] = (counts[answer] || 0) + 1 // increment answer count
+          return counts
+        }, {})
 
-        var barColor;
-
-        const chartData = Object.keys(answerCounts).map((key, index) => {
-          const value = answerCounts[key];
-          if(key === 'verde'){
-            barColor = "#00FF00";
-          }
-          if(key === 'rojo'){
-            barColor = "#FF0000";
-          }
-          if(key === 'azul'){
-            barColor = "#0000FF";
-          }
+        const chartData = Object.keys(answerCounts).map((key) => {
+          const value = answerCounts[key]
           return {
             name: key,
-            value,
-          };
-        });
+            value
+          }
+        })
         chartData.sort((a, b) => {
-          const order = ["verde", "rojo", "azul"];
-          return order.indexOf(a.name) - order.indexOf(b.name);
-        });
+          const order = ['verde', 'rojo', 'azul']
+          return order.indexOf(a.name) - order.indexOf(b.name)
+        })
 
         return (
           <Flex
             key={index}
-            w={"auto"}
+            w={'auto'}
             flexDirection="column"
-            p={1}
-            align={"start"}
+            p={4}
+            m={4}
+            maxW={500}
+            align={'start'}
           >
             <Text fontSize="xl" fontWeight="bold">
               {title}
@@ -56,13 +56,20 @@ const QuestionChart = ({ data }) => {
               <XAxis dataKey="name" />
               <YAxis />
               <Tooltip />
-              <Bar dataKey="value" fill={barColor} />
+              <Bar dataKey="value">
+                {chartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={colors[index % colors.length]}
+                  />
+                ))}
+              </Bar>
             </BarChart>
           </Flex>
-        );
+        )
       })}
     </Flex>
-  );
-};
+  )
+}
 
-export default QuestionChart;
+export default QuestionChart
