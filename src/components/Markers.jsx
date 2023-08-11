5ea1529f10b0d65d52bffd87c4e47f4c62dc4120
josafat <josa221001@gmail.com>
2023-08-10 19:47:25 -0600

    updt: ajustando el tamaño del marker

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -1,65 +1,65 @@
-import { Marker, Popup } from 'react-leaflet'
-import { useEffect, useState, useMemo } from 'react'
+import { Marker, Popup } from "react-leaflet";
+import { useEffect, useState, useMemo } from "react";
 
 const Markers = ({ selectedQuestion }) => {
-  const [markers, setMarkers] = useState([])
+  const [markers, setMarkers] = useState([]);
 
   const colorMap = useMemo(() => {
     return {
-      azul: 'blue',
-      rojo: 'red',
-      verde: 'green'
-    }
-  }, [])
+      azul: "blue",
+      rojo: "red",
+      verde: "green",
+    };
+  }, []);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch(
           `${import.meta.env.VITE_PUBLIC_DATA_ALL}/all`
-        )
-        const data = await response.json()
-        console.log('data', data)
+        );
+        const data = await response.json();
+        console.log("data", data);
 
-        setMarkers(data)
+        setMarkers(data);
       } catch (error) {
-        console.log('Error al obtener los datos:', error)
+        console.log("Error al obtener los datos:", error);
       }
     }
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const filteredMarkers = markers.filter((marker) => {
     const selectedResponse = marker.responses.find(
       (response) => Object.keys(response)[0] === selectedQuestion
-    )
-    return selectedResponse !== undefined
-  })
+    );
+    return selectedResponse !== undefined;
+  });
 
   return (
     <>
       {filteredMarkers.map((marker) => {
         const selectedResponse = marker.responses.find(
           (response) => Object.keys(response)[0] === selectedQuestion
-        )
+        );
         const color =
           selectedResponse !== undefined
-            ? colorMap[selectedResponse[selectedQuestion]] || 'gray'
-            : 'gray'
+            ? colorMap[selectedResponse[selectedQuestion]] || "gray"
+            : "gray";
         const answer = selectedResponse
           ? selectedResponse[selectedQuestion]
-          : ''
+          : "";
         return (
           <Marker
             key={marker.id}
             position={[marker.ubication.lat, marker.ubication.long]}
             icon={
               new L.Icon({
-                iconUrl: '/icons/' + color + '.svg',
-                iconSize: [40, 46],
+                iconUrl: "/icons/" + color + ".svg",
+                iconSize: [13, 19],
                 iconAnchor: [14, 12],
                 popupAnchor: [6, -8],
-                shadowAnchor: [4, 62]
+                shadowAnchor: [4, 62],
               })
             }
           >
@@ -71,10 +71,10 @@ const Markers = ({ selectedQuestion }) => {
               {`Longitud: ${marker.ubication.long}`}
             </Popup>
           </Marker>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
 
-export default Markers
+export default Markers;
